b89039947d764273cb40027566e5b0d618387d23
unknown <pocciaeugenio@gmail.com>
2019-08-04 00:37:42 -0400

    blacked out dates on calendar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,23 +6,46 @@ import 'react-dates/lib/css/_datepicker.css';
 import moment from 'moment'
 
 
-
 class UnconnectedApp extends Component {
   constructor(props){
     super(props)
+    this.isDayBlocked = this.isDayBlocked.bind(this)
+    this.componentDidMount = this.componentDidMount.bind(this)
+
     this.state = {
       date: null,
-      focused: null
+      focused: null,
+      datesTaken: {test: 'hello'}
     }
   }
 
+
+  //GET taken dates
+componentDidMount = () => {
+  fetch('http://localhost:4000/get-dates')
+  .then(x => {return x.text()})
+  .then(responseBody => {
+    let body = JSON.parse(responseBody)
+    if (body.success) {
+      console.log('body, ' ,body)
+      this.setState({datesTaken: body.datesTaken})
+    }
+  })
+}
+
+isDayBlocked(date) {
+if (this.state.datesTaken[date] === true) {
   
+  return true
+}
+}
+
 
 //send data to backend
   handleReserve = () => {
     let data = new FormData();
     data.append('date', this.state.date)    
-  
+
     fetch('http://localhost:4000/post-dates',
     {
       method: "POST",
@@ -34,32 +57,30 @@ class UnconnectedApp extends Component {
       let body = JSON.parse(responseBody)
      if (body.success) {
        alert('date added to backend')
-      console.log(body.dummyTest)
+      
       }
-
     })
-
- console.log('in button')
-      console.log(this.state.date)
-      console.log(this.state.date._d)
-
       let timeNow = this.state.date._d
       console.log('reserved, ' , moment(timeNow).format('Do MMM YYYY'))
+      
+       }
 
-  }
+
   render = () => {
      return (
     <div >
     <SingleDatePicker
-  date={this.state.date} // momentPropTypes.momentObj or null
-  onDateChange={date => this.setState({ date })} // PropTypes.func.isRequired
-  focused={this.state.focused} // PropTypes.bool
-  onFocusChange={({ focused }) => this.setState({ focused })} // PropTypes.func.isRequired
-  id="your_unique_id" // PropTypes.string.isRequired,
-/>
-<div>
-  <button onClick={this.handleReserve}>Reserve</button>
-  </div>
+     date={this.state.date} 
+     onDateChange={date => this.setState({ date })} 
+     focused={this.state.focused} 
+     onFocusChange={({ focused }) => this.setState({ focused })} 
+     isDayBlocked={this.isDayBlocked}
+     id="your_unique_id" />
+  
+    <div>
+    <button onClick={this.handleReserve}>Reserve</button>
+    </div>
+    
     </div>);
     }
 }
